066e9c2737919d8aa79b531378d5a3886325f699
methlox <astroyellowh@gmail.com>
2023-09-20 14:53:13 +0530

    getting photos by id

--- src/main/java/com/sid/gallery/GalleryController.java
+++ src/main/java/com/sid/gallery/GalleryController.java
@@ -1,20 +1,37 @@
 package com.sid.gallery;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.net.http.HttpResponse;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class GalleryController {
 
-    private List<Photos> db = List.of(new Photos("1", "hello.jpg"));
+    private Map<String, Photos> db = new HashMap<>(){{ put("1", new Photos("1", "hello.jpg"));
+    }};
     @GetMapping("/")
     public String hello() {
         return "Hello Sid";
     }
 
     @GetMapping("/photos")
-    public List<Photos> get() {
-        return db;
+    public Collection<Photos> get() {
+        return db.values();
+    }
+
+    @GetMapping("/photos/{id}")
+    public Photos get(@PathVariable String id) {
+        Photos photos = db.get(id);
+        if (photos == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photos;
     }
 }
